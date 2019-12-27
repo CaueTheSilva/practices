1ed64c36473c078afb4ea562374439b3a76f61a7
Cauê <cauescl@gmail.com>
2019-12-27 01:05:29 -0300

    initial commit: magic8ball.py

---
+++ magic8ball.py
@@ -0,0 +1,29 @@
+import time
+import random
+
+while True:
+    alist = ["Yes","No","Maybe","Wait and see","Best not to answer now","Probably",
+            "There is a possibility", "The odds are in your favor", "Probably not",
+            "The future is still cloudy", "Not sure", "Maybe tomorrow", "Not today",
+            "Good luck with that", "You can certainly try",
+            "How do you want to do this", "Wouldn't you like to know, weather boy",
+            "It'll end in cake", "Who cares?", "42"]
+
+    input("Enter a question: ")
+
+    for i in range(5):
+        print("Thinking...")
+        time.sleep(1)
+
+    print("\n")
+
+    print("Answer: " + str(alist[random.randint(0, 19)]))
+
+    answer = input("Would you like to ask another question? Answer with y for Yes or n for No. \n")
+
+    if answer == "y":
+        pass
+    elif answer == "n":
+        break
+    else:
+        print("Please enter a valid answer.")
